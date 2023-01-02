f922251bbe2d1ccd40516ffdf7324450fb0d63c1
Mikhail Starikov <michail.a.starikov@gmail.com>
2023-01-02 18:27:58 +0100

    upd portfolio website

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }
     ]
   },
   components: {
